940f18b37f4352d2737e8e23ebe4108f95cbc058
Alex Antipin <alex@antipin.com>
2013-06-05 05:03:27 +0400

    Добавлен блок b-section

---
+++ blocks/b-section/b-section.tpl.php
@@ -0,0 +1,9 @@
+<section class="<?= $block["block"] ?>">
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+                <?= $block["content"] ?>
+            </div>
+        </div>
+    </div>
+</section>
