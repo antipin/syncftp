56e1089cbdaf1211bbb900b57ae2144f6ec4c96d
Alex Antipin <alex@antipin.com>
2013-06-05 05:04:26 +0400

    Добавлен блок b-title

---
+++ blocks/b-title/b-title.tpl.php
@@ -0,0 +1,16 @@
+<section class="<?= $block["block"] ?>">
+    <div class="container">
+        <div class="row">
+
+            <h1 class="b-title__title span4">
+                <?=  $block["title"]; ?>
+            </h1>
+
+            <div class="b-title__teaser span8">
+                <?=  $block["content"]; ?>
+            </div>
+
+        </div>
+
+    </div>
+</section>
