6cb3d853a2309fb160ed6a30de327f9e63315a6b
Alex Antipin <alex@antipin.com>
2013-06-06 03:07:39 +0400

    Удален лишний отступ на странице 404.

--- pages/404/404.tree.php
+++ pages/404/404.tree.php
@@ -2,29 +2,29 @@
 
 return array(
 
-        "headers" => array(
-            "HTTP/1.0 404 Not Found"
-        ),
+    "headers" => array(
+        "HTTP/1.0 404 Not Found"
+    ),
 
-        "page" => array(
-            "block" => "b-page",
-            "content" => array(
-                array(
-                    "block" => "b-header",
-                    "content" => array(
-                        "block" => "b-nav",
-                        "content" => include(DIR_BLOCKS . "/b-nav/b-nav.tree.php") // todo: сделать поиск автоматическим
-                    ),
-                ),
-
-                array(
-                    "block" => "b-section",
-                    "content" => "404. Page not found"
-                ),
-
-                array(
-                    "block" => "b-footer"
+    "page" => array(
+        "block" => "b-page",
+        "content" => array(
+            array(
+                "block" => "b-header",
+                "content" => array(
+                    "block" => "b-nav",
+                    "content" => include(DIR_BLOCKS . "/b-nav/b-nav.tree.php") // todo: сделать поиск автоматическим
                 ),
             ),
-        )
-    );
+
+            array(
+                "block" => "b-section",
+                "content" => "404. Page not found"
+            ),
+
+            array(
+                "block" => "b-footer"
+            ),
+        ),
+    )
+);
