5b5763436f4b792a22b7f53a9de99bd3c4112d70
Alex Antipin <alex@antipin.com>
2013-06-10 04:20:53 +0400

    Реализована подсветка текущего пункта меню.

--- blocks/b-nav-item/b-nav-item.tpl.php
+++ blocks/b-nav-item/b-nav-item.tpl.php
@@ -1,1 +1,8 @@
-<a href="/<?= $block["href"] ?>" class="<?= $block["block"] ?>"><?= $block["content"] ?></a>
+<?php
+global $router;
+$currentRoute = $router->get_route();
+if ($currentRoute["request"] == trim($block["href"], '/')) {
+    $classActive = "b-nav-item_active_yes";
+}
+?>
+<a href="/<?= $block["href"] ?>" class="<?= $classActive; ?> <?= $block["block"]; ?>"><?= $block["content"] ?></a>
